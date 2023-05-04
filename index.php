30d3568ae2bfd4c6c794ecc7cf4ff1b44f283238
Somik <github@gemx.us>
2023-05-04 10:19:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ else{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mini Image Host | ThinPCB</title>
+    <title>Mini Image Host</title>
     <link rel="stylesheet" href="style.css<?=$v?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
