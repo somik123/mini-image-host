3522eda9e76cc6a6e8e889ad6feb9c1654c3fc8f
Somik <github@gemx.us>
2023-05-05 11:13:41 +0800

    Fixed typo

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ else{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mini Image Host | ImagePerl</title>
+    <title>Mini Image Host</title>
     <link rel="stylesheet" href="style.css<?=$v?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
