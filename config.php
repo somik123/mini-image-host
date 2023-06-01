d2ec040cd1751321c9d75a77c9091d1649812d8e
Somik <github@gemx.us>
2023-06-01 12:51:49 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 $protocol = "https://";
 $domain = "img.ec.gy";
 
-$upload_path = "/var/www/img.ec.gy/public_html/";
+$upload_path = "/var/www/{$domain}/public_html/";
 $url_path = "/";
 
-$mirror_list = array("img.ec.gy"=>"","img.42.ar"=>"img.hooks.cc");
+$mirror_list = array($domain=>"");
 
 $max_filesize = 5 * 1024 * 1024;
