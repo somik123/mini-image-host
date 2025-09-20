f97e4139b402e719e99b12cbdc8af595b16e3eb1
Somik <somik123@users.noreply.github.com>
2025-09-20 17:42:54 +0800

    Update index.php

--- html/index.php
+++ html/index.php
@@ -19,7 +19,7 @@ $max_filesize_msg = human_readable_size($max_file_size, 0);
 
 <head>
     <meta charset="UTF-8">
-    <title>Mini Image Host - <?= $domain ?></title>
+    <title><?=$site_name?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
